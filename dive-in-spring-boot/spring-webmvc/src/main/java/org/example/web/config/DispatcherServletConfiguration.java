6d665b24adf83873d94c8dabf6a2cbce7aa907ba
longmin LI <1090744429@qq.com>
2022-09-15 21:47:34 +0800

    'local/study-2'

---
+++ dive-in-spring-boot/spring-webmvc/src/main/java/org/example/web/config/DispatcherServletConfiguration.java
@@ -0,0 +1,13 @@
+package org.example.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+/**
+ * @auhtor llm
+ * @data 2022/9/15 20:13
+ */
+@ComponentScan(basePackages = "org.example.web")
+public class DispatcherServletConfiguration {
+
+}
